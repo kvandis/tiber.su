f6c71b96a6f7e985f774df5f6bbf0d95601daf77
kvandis <alex.mezh@uavos.com>
2022-12-10 07:41:48 +0300

    upd

---
+++ index7b17.php
@@ -0,0 +1,113 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<feed xmlns="http://www.w3.org/2005/Atom"  xml:lang="en-gb">
+	<title type="text">Creative multipurpose Joomla template for business - JA Morgan</title>
+	<subtitle type="text"></subtitle>
+	<link rel="alternate" type="text/html" href="http://morgan"/>
+	<id>http://morgan/index.php</id>
+	<updated>2022-12-10T04:31:21+00:00</updated>
+	<generator uri="https://www.joomla.org">Joomla! - Open Source Content Management</generator>
+	<link rel="self" type="application/atom+xml" href="http://morgan/index.php?format=feed&amp;type=atom"/>
+	<entry>
+		<title>BASF Issues Profit Warning on U.S.-China Trade Concerns Longe and Parole</title>
+		<link rel="alternate" type="text/html" href="http://morgan/index.php/joomlart-content/category-blog/6-basf-issues-profit-warning-on-u-s-china-trade-concerns-longe-and-parole"/>
+		<published>2022-12-10T01:24:22+00:00</published>
+		<updated>2022-12-10T01:24:22+00:00</updated>
+		<id>http://morgan/index.php/joomlart-content/category-blog/6-basf-issues-profit-warning-on-u-s-china-trade-concerns-longe-and-parole</id>
+		<author>
+			<name>Super User</name>
+		</author>
+		<summary type="html">&lt;div class=&quot;feed-description&quot;&gt;&lt;p&gt;&lt;img src=&quot;http://morgan/images/joomlart/blog/img-18-thumb.jpg&quot; /&gt;&lt;/p&gt;&lt;p&gt;But nothing the copy said could convince her and so it didn’t take long. But nothing the copy said could her and so it didn’t take long. Copy Writers ambushed her, made her drunk.&lt;/p&gt;
+&lt;/div&gt;</summary>
+		<content type="html">&lt;div class=&quot;feed-description&quot;&gt;&lt;p&gt;&lt;img src=&quot;http://morgan/images/joomlart/blog/img-18-thumb.jpg&quot; /&gt;&lt;/p&gt;&lt;p&gt;But nothing the copy said could convince her and so it didn’t take long. But nothing the copy said could her and so it didn’t take long. Copy Writers ambushed her, made her drunk.&lt;/p&gt;
+&lt;/div&gt;</content>
+		<category term="Featured" />
+		<category term="Investment Managment" />
+		<category term="Blog" />
+	</entry>
+	<entry>
+		<title>This is what super savers who plan to retire early are doing differently</title>
+		<link rel="alternate" type="text/html" href="http://morgan/index.php/joomlart-content/category-blog/7-this-is-what-super-savers-who-plan-to-retire-early-are-doing-differently"/>
+		<published>2022-12-10T01:24:22+00:00</published>
+		<updated>2022-12-10T01:24:22+00:00</updated>
+		<id>http://morgan/index.php/joomlart-content/category-blog/7-this-is-what-super-savers-who-plan-to-retire-early-are-doing-differently</id>
+		<author>
+			<name>Super User</name>
+		</author>
+		<summary type="html">&lt;div class=&quot;feed-description&quot;&gt;&lt;p&gt;&lt;img src=&quot;http://morgan/images/joomlart/blog/img-3-thumb.jpg&quot; /&gt;&lt;/p&gt;&lt;p&gt;Pityful a rethoric question ran over her cheek, then she continued her way. On her way she met a copy, then she continued her way of the Semantics, a large language ocean.&lt;/p&gt;
+&lt;/div&gt;</summary>
+		<content type="html">&lt;div class=&quot;feed-description&quot;&gt;&lt;p&gt;&lt;img src=&quot;http://morgan/images/joomlart/blog/img-3-thumb.jpg&quot; /&gt;&lt;/p&gt;&lt;p&gt;Pityful a rethoric question ran over her cheek, then she continued her way. On her way she met a copy, then she continued her way of the Semantics, a large language ocean.&lt;/p&gt;
+&lt;/div&gt;</content>
+		<category term="Featured" />
+		<category term="Investment Managment" />
+		<category term="Blog" />
+	</entry>
+	<entry>
+		<title>The world needs to get serious about combating carbon emissions</title>
+		<link rel="alternate" type="text/html" href="http://morgan/index.php/joomlart-content/category-blog/8-the-world-needs-to-get-serious-about-combating-carbon-emissions"/>
+		<published>2022-12-10T01:24:22+00:00</published>
+		<updated>2022-12-10T01:24:22+00:00</updated>
+		<id>http://morgan/index.php/joomlart-content/category-blog/8-the-world-needs-to-get-serious-about-combating-carbon-emissions</id>
+		<author>
+			<name>Super User</name>
+		</author>
+		<summary type="html">&lt;div class=&quot;feed-description&quot;&gt;&lt;p&gt;&lt;img src=&quot;http://morgan/images/joomlart/blog/img-15-thumb.jpg&quot; /&gt;&lt;/p&gt;&lt;p&gt;Pityful a rethoric question ran over her cheek, then she continued her way, times and everything that was left. Pityful a rethoric question her cheek, from a thousand times.&lt;/p&gt;
+&lt;/div&gt;</summary>
+		<content type="html">&lt;div class=&quot;feed-description&quot;&gt;&lt;p&gt;&lt;img src=&quot;http://morgan/images/joomlart/blog/img-15-thumb.jpg&quot; /&gt;&lt;/p&gt;&lt;p&gt;Pityful a rethoric question ran over her cheek, then she continued her way, times and everything that was left. Pityful a rethoric question her cheek, from a thousand times.&lt;/p&gt;
+&lt;/div&gt;</content>
+		<category term="Featured" />
+		<category term="Investment Managment" />
+		<category term="Blog" />
+	</entry>
+	<entry>
+		<title>Extreme Trash Recycling: 104 Protocols, 30,000 Inspectors Behind The Word Mountains</title>
+		<link rel="alternate" type="text/html" href="http://morgan/index.php/joomlart-content/list-all-categories/9-business-growth-advice/14-extreme-trash-recycling-104-protocols-30-000-inspectors-behind-the-word-mountains"/>
+		<published>2022-12-10T01:24:22+00:00</published>
+		<updated>2022-12-10T01:24:22+00:00</updated>
+		<id>http://morgan/index.php/joomlart-content/list-all-categories/9-business-growth-advice/14-extreme-trash-recycling-104-protocols-30-000-inspectors-behind-the-word-mountains</id>
+		<author>
+			<name>Super User</name>
+		</author>
+		<summary type="html">&lt;div class=&quot;feed-description&quot;&gt;&lt;p&gt;&lt;img src=&quot;http://morgan/images/joomlart/blog/img-6-thumb.jpg&quot; /&gt;&lt;/p&gt;&lt;p&gt;In his bed into a horrible vermin. Far far away, behind the word mountains, far from the countries a horrible vermin.&lt;/p&gt;
+&lt;/div&gt;</summary>
+		<content type="html">&lt;div class=&quot;feed-description&quot;&gt;&lt;p&gt;&lt;img src=&quot;http://morgan/images/joomlart/blog/img-6-thumb.jpg&quot; /&gt;&lt;/p&gt;&lt;p&gt;In his bed into a horrible vermin. Far far away, behind the word mountains, far from the countries a horrible vermin.&lt;/p&gt;
+&lt;/div&gt;</content>
+		<category term="Featured" />
+		<category term="Business Growth Advice" />
+		<category term="Blog" />
+	</entry>
+	<entry>
+		<title>China’s Newly Created Dajia Insurance Begins Absorbing Anbang</title>
+		<link rel="alternate" type="text/html" href="http://morgan/index.php/joomlart-content/list-all-categories/9-business-growth-advice/15-china-s-newly-created-dajia-insurance-begins-absorbing-anbang"/>
+		<published>2022-12-10T01:24:22+00:00</published>
+		<updated>2022-12-10T01:24:22+00:00</updated>
+		<id>http://morgan/index.php/joomlart-content/list-all-categories/9-business-growth-advice/15-china-s-newly-created-dajia-insurance-begins-absorbing-anbang</id>
+		<author>
+			<name>Super User</name>
+		</author>
+		<summary type="html">&lt;div class=&quot;feed-description&quot;&gt;&lt;p&gt;&lt;img src=&quot;http://morgan/images/joomlart/blog/img-9-thumb.jpg&quot; /&gt;&lt;/p&gt;&lt;p&gt;Gregor then turned to look out the window at the dull weather. Drops of rain could be heard hitting the pane, which made Bookmarksgrove right at the coast of the Semantics.&lt;/p&gt;
+&lt;/div&gt;</summary>
+		<content type="html">&lt;div class=&quot;feed-description&quot;&gt;&lt;p&gt;&lt;img src=&quot;http://morgan/images/joomlart/blog/img-9-thumb.jpg&quot; /&gt;&lt;/p&gt;&lt;p&gt;Gregor then turned to look out the window at the dull weather. Drops of rain could be heard hitting the pane, which made Bookmarksgrove right at the coast of the Semantics.&lt;/p&gt;
+&lt;/div&gt;</content>
+		<category term="Featured" />
+		<category term="Business Growth Advice" />
+		<category term="Blog" />
+	</entry>
+	<entry>
+		<title>Pelosi’s Sharp Message for Democrats Finds Receptive Audience </title>
+		<link rel="alternate" type="text/html" href="http://morgan/index.php/joomlart-content/list-all-categories/10-tax-advice-and-management/17-pelosi-s-sharp-message-for-democrats-finds-receptive-audience"/>
+		<published>2022-12-10T01:24:22+00:00</published>
+		<updated>2022-12-10T01:24:22+00:00</updated>
+		<id>http://morgan/index.php/joomlart-content/list-all-categories/10-tax-advice-and-management/17-pelosi-s-sharp-message-for-democrats-finds-receptive-audience</id>
+		<author>
+			<name>Super User</name>
+		</author>
+		<summary type="html">&lt;div class=&quot;feed-description&quot;&gt;&lt;p&gt;&lt;img src=&quot;http://morgan/images/joomlart/blog/img-16-thumb.jpg&quot; /&gt;&lt;/p&gt;&lt;p&gt;However hard he threw himself onto his right, he always rolled back to where he was. He must have tried it a hundred times, behind the there live the blind texts.&lt;/p&gt;
+&lt;/div&gt;</summary>
+		<content type="html">&lt;div class=&quot;feed-description&quot;&gt;&lt;p&gt;&lt;img src=&quot;http://morgan/images/joomlart/blog/img-16-thumb.jpg&quot; /&gt;&lt;/p&gt;&lt;p&gt;However hard he threw himself onto his right, he always rolled back to where he was. He must have tried it a hundred times, behind the there live the blind texts.&lt;/p&gt;
+&lt;/div&gt;</content>
+		<category term="Featured" />
+		<category term="Tax Advice &amp; Management" />
+		<category term="Blog" />
+	</entry>
+</feed>
---
+++ indexc0d0.php
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+	<channel>
+		<title>Creative multipurpose Joomla template for business - JA Morgan</title>
+		<description><![CDATA[]]></description>
+		<link>http://morgan/index.php</link>
+		<lastBuildDate>Sat, 10 Dec 2022 04:31:21 +0000</lastBuildDate>
+		<generator>Joomla! - Open Source Content Management</generator>
+		<atom:link rel="self" type="application/rss+xml" href="http://morgan/index.php?format=feed&amp;type=rss"/>
+		<language>en-gb</language>
+		<item>
+			<title>BASF Issues Profit Warning on U.S.-China Trade Concerns Longe and Parole</title>
+			<link>http://morgan/index.php/joomlart-content/category-blog/6-basf-issues-profit-warning-on-u-s-china-trade-concerns-longe-and-parole</link>
+			<guid isPermaLink="true">http://morgan/index.php/joomlart-content/category-blog/6-basf-issues-profit-warning-on-u-s-china-trade-concerns-longe-and-parole</guid>
+			<description><![CDATA[<div class="feed-description"><p><img src="http://morgan/images/joomlart/blog/img-18-thumb.jpg" /></p><p>But nothing the copy said could convince her and so it didn’t take long. But nothing the copy said could her and so it didn’t take long. Copy Writers ambushed her, made her drunk.</p>
+</div>]]></description>
+			<category>Featured</category>
+			<category>Investment Managment</category>
+			<category>Blog</category>
+			<pubDate>Sat, 10 Dec 2022 01:24:22 +0000</pubDate>
+		</item>
+		<item>
+			<title>This is what super savers who plan to retire early are doing differently</title>
+			<link>http://morgan/index.php/joomlart-content/category-blog/7-this-is-what-super-savers-who-plan-to-retire-early-are-doing-differently</link>
+			<guid isPermaLink="true">http://morgan/index.php/joomlart-content/category-blog/7-this-is-what-super-savers-who-plan-to-retire-early-are-doing-differently</guid>
+			<description><![CDATA[<div class="feed-description"><p><img src="http://morgan/images/joomlart/blog/img-3-thumb.jpg" /></p><p>Pityful a rethoric question ran over her cheek, then she continued her way. On her way she met a copy, then she continued her way of the Semantics, a large language ocean.</p>
+</div>]]></description>
+			<category>Featured</category>
+			<category>Investment Managment</category>
+			<category>Blog</category>
+			<pubDate>Sat, 10 Dec 2022 01:24:22 +0000</pubDate>
+		</item>
+		<item>
+			<title>The world needs to get serious about combating carbon emissions</title>
+			<link>http://morgan/index.php/joomlart-content/category-blog/8-the-world-needs-to-get-serious-about-combating-carbon-emissions</link>
+			<guid isPermaLink="true">http://morgan/index.php/joomlart-content/category-blog/8-the-world-needs-to-get-serious-about-combating-carbon-emissions</guid>
+			<description><![CDATA[<div class="feed-description"><p><img src="http://morgan/images/joomlart/blog/img-15-thumb.jpg" /></p><p>Pityful a rethoric question ran over her cheek, then she continued her way, times and everything that was left. Pityful a rethoric question her cheek, from a thousand times.</p>
+</div>]]></description>
+			<category>Featured</category>
+			<category>Investment Managment</category>
+			<category>Blog</category>
+			<pubDate>Sat, 10 Dec 2022 01:24:22 +0000</pubDate>
+		</item>
+		<item>
+			<title>Extreme Trash Recycling: 104 Protocols, 30,000 Inspectors Behind The Word Mountains</title>
+			<link>http://morgan/index.php/joomlart-content/list-all-categories/9-business-growth-advice/14-extreme-trash-recycling-104-protocols-30-000-inspectors-behind-the-word-mountains</link>
+			<guid isPermaLink="true">http://morgan/index.php/joomlart-content/list-all-categories/9-business-growth-advice/14-extreme-trash-recycling-104-protocols-30-000-inspectors-behind-the-word-mountains</guid>
+			<description><![CDATA[<div class="feed-description"><p><img src="http://morgan/images/joomlart/blog/img-6-thumb.jpg" /></p><p>In his bed into a horrible vermin. Far far away, behind the word mountains, far from the countries a horrible vermin.</p>
+</div>]]></description>
+			<category>Featured</category>
+			<category>Business Growth Advice</category>
+			<category>Blog</category>
+			<pubDate>Sat, 10 Dec 2022 01:24:22 +0000</pubDate>
+		</item>
+		<item>
+			<title>China’s Newly Created Dajia Insurance Begins Absorbing Anbang</title>
+			<link>http://morgan/index.php/joomlart-content/list-all-categories/9-business-growth-advice/15-china-s-newly-created-dajia-insurance-begins-absorbing-anbang</link>
+			<guid isPermaLink="true">http://morgan/index.php/joomlart-content/list-all-categories/9-business-growth-advice/15-china-s-newly-created-dajia-insurance-begins-absorbing-anbang</guid>
+			<description><![CDATA[<div class="feed-description"><p><img src="http://morgan/images/joomlart/blog/img-9-thumb.jpg" /></p><p>Gregor then turned to look out the window at the dull weather. Drops of rain could be heard hitting the pane, which made Bookmarksgrove right at the coast of the Semantics.</p>
+</div>]]></description>
+			<category>Featured</category>
+			<category>Business Growth Advice</category>
+			<category>Blog</category>
+			<pubDate>Sat, 10 Dec 2022 01:24:22 +0000</pubDate>
+		</item>
+		<item>
+			<title>Pelosi’s Sharp Message for Democrats Finds Receptive Audience </title>
+			<link>http://morgan/index.php/joomlart-content/list-all-categories/10-tax-advice-and-management/17-pelosi-s-sharp-message-for-democrats-finds-receptive-audience</link>
+			<guid isPermaLink="true">http://morgan/index.php/joomlart-content/list-all-categories/10-tax-advice-and-management/17-pelosi-s-sharp-message-for-democrats-finds-receptive-audience</guid>
+			<description><![CDATA[<div class="feed-description"><p><img src="http://morgan/images/joomlart/blog/img-16-thumb.jpg" /></p><p>However hard he threw himself onto his right, he always rolled back to where he was. He must have tried it a hundred times, behind the there live the blind texts.</p>
+</div>]]></description>
+			<category>Featured</category>
+			<category>Tax Advice &amp; Management</category>
+			<category>Blog</category>
+			<pubDate>Sat, 10 Dec 2022 01:24:22 +0000</pubDate>
+		</item>
+	</channel>
+</rss>
